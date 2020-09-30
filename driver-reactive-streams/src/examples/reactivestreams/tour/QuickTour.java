7fefa47d6fed6de49f81489b9406abca9b9caa28
Brian DeLeonardis <bdeleonardis1@gmail.com>
2020-09-30 15:14:13 -0400

    Setup

--- driver-reactive-streams/src/examples/reactivestreams/tour/QuickTour.java
+++ driver-reactive-streams/src/examples/reactivestreams/tour/QuickTour.java
@@ -17,40 +17,14 @@
 
 package reactivestreams.tour;
 
-import com.mongodb.client.result.DeleteResult;
-import com.mongodb.client.result.InsertManyResult;
-import com.mongodb.client.result.InsertOneResult;
-import com.mongodb.client.result.UpdateResult;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoCollection;
-import com.mongodb.reactivestreams.client.MongoDatabase;
-import reactivestreams.helpers.SubscriberHelpers.ObservableSubscriber;
-import reactivestreams.helpers.SubscriberHelpers.OperationSubscriber;
-import reactivestreams.helpers.SubscriberHelpers.PrintDocumentSubscriber;
-import reactivestreams.helpers.SubscriberHelpers.PrintSubscriber;
-import org.bson.Document;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.mongodb.client.model.Accumulators.sum;
-import static com.mongodb.client.model.Aggregates.group;
-import static com.mongodb.client.model.Aggregates.match;
-import static com.mongodb.client.model.Aggregates.project;
-import static com.mongodb.client.model.Filters.and;
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.exists;
-import static com.mongodb.client.model.Filters.gt;
-import static com.mongodb.client.model.Filters.gte;
-import static com.mongodb.client.model.Filters.lt;
-import static com.mongodb.client.model.Filters.lte;
-import static com.mongodb.client.model.Projections.excludeId;
-import static com.mongodb.client.model.Sorts.descending;
-import static com.mongodb.client.model.Updates.inc;
-import static com.mongodb.client.model.Updates.set;
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The QuickTour code example
@@ -62,132 +36,60 @@ public class QuickTour {
      * @param args takes an optional single argument for the connection string
      */
     public static void main(final String[] args) {
-        MongoClient mongoClient;
+        try (MongoClient client = MongoClients.create()) {
+            ReactiveMongoTemplate template = new ReactiveMongoTemplate(client, "test");
 
-        if (args.length == 0) {
-            // connect to the local database server
-            mongoClient = MongoClients.create();
-        } else {
-            mongoClient = MongoClients.create(args[0]);
+            String tel = "111111";
+            System.out.println("Before save");
+            Map<String, Object> data = new HashMap<>(Map.of("tel", tel, "child", Map.of("name", "Lily")));
+            template.save(data, template.getCollectionName(Family.class)).block();
+
+            System.out.println("After save");
+
+            template.findOne(Query.query(Criteria.where("tel").is(tel)), Family.class)
+                    .doOnError(Throwable::printStackTrace)
+                    .block();
+
+            System.out.println("After findOne");
+        }
+    }
+
+    public static abstract class Child {
+        private String name = "Unknown";
+
+        public String getName() {
+            return name;
         }
 
-        // get handle to "mydb" database
-        MongoDatabase database = mongoClient.getDatabase("mydb");
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 
-        // get a handle to the "test" collection
-        final MongoCollection<Document> collection = database.getCollection("test");
+    public static final class Son extends Child {
 
-        // drop all the data in it
-        ObservableSubscriber<Void> successSubscriber = new OperationSubscriber<>();
-        collection.drop().subscribe(successSubscriber);
-        successSubscriber.await();
+    }
 
-        // make a document and insert it
-        Document doc = new Document("name", "MongoDB")
-                .append("type", "database")
-                .append("count", 1)
-                .append("info", new Document("x", 203).append("y", 102));
+    public static final class Family {
 
-        ObservableSubscriber<InsertOneResult> insertOneSubscriber = new OperationSubscriber<>();
-        collection.insertOne(doc).subscribe(insertOneSubscriber);
-        insertOneSubscriber.await();
+        private String tel = "000000";
 
-        // get it (since it's the only one in there since we dropped the rest earlier on)
-        ObservableSubscriber<Document> documentSubscriber = new PrintDocumentSubscriber();
-        collection.find().first().subscribe(documentSubscriber);
-        documentSubscriber.await();
+        private Child child = new Son();
 
-        // now, lets add lots of little documents to the collection so we can explore queries and cursors
-        List<Document> documents = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            documents.add(new Document("i", i));
+        public String getTel() {
+            return tel;
         }
 
-        ObservableSubscriber<InsertManyResult> insertManySubscriber = new OperationSubscriber<>();
-        collection.insertMany(documents).subscribe(insertManySubscriber);
-        insertManySubscriber.await();
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
 
-        // find first
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find().first().subscribe(documentSubscriber);
-        documentSubscriber.await();
+        public Child getChild() {
+            return child;
+        }
 
-        // lets get all the documents in the collection and print them out
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find().subscribe(documentSubscriber);
-        documentSubscriber.await();
-
-        // Query Filters
-
-        // now use a query to get 1 document out
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find(eq("i", 71)).first().subscribe(documentSubscriber);
-        documentSubscriber.await();
-
-        // now use a range query to get a larger subset
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find(gt("i", 50)).subscribe(documentSubscriber);
-        successSubscriber.await();
-
-        // range query with multiple constraints
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find(and(gt("i", 50), lte("i", 100))).subscribe(documentSubscriber);
-        successSubscriber.await();
-
-        // Sorting
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find(exists("i")).sort(descending("i")).first().subscribe(documentSubscriber);
-        documentSubscriber.await();
-
-        // Projection
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.find().projection(excludeId()).first().subscribe(documentSubscriber);
-        documentSubscriber.await();
-
-        // Aggregation
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.aggregate(asList(
-                match(gt("i", 0)),
-                project(Document.parse("{ITimes10: {$multiply: ['$i', 10]}}")))
-        ).subscribe(documentSubscriber);
-        documentSubscriber.await();
-
-        documentSubscriber = new PrintDocumentSubscriber();
-        collection.aggregate(singletonList(group(null, sum("total", "$i"))))
-                .first().subscribe(documentSubscriber);
-        documentSubscriber.await();
-
-        // Update One
-        ObservableSubscriber<UpdateResult> updateSubscriber = new OperationSubscriber<>();
-        collection.updateOne(eq("i", 10), set("i", 110)).subscribe(updateSubscriber);
-        updateSubscriber.await();
-
-        // Update Many
-        updateSubscriber = new OperationSubscriber<>();
-        collection.updateMany(lt("i", 100), inc("i", 100)).subscribe(updateSubscriber);
-        updateSubscriber.await();
-
-        // Delete One
-        ObservableSubscriber<DeleteResult> deleteSubscriber = new OperationSubscriber<>();
-        collection.deleteOne(eq("i", 110)).subscribe(deleteSubscriber);
-        deleteSubscriber.await();
-
-        // Delete Many
-        deleteSubscriber = new OperationSubscriber<>();
-        collection.deleteMany(gte("i", 100)).subscribe(deleteSubscriber);
-        deleteSubscriber.await();
-
-        // Create Index
-        OperationSubscriber<String> createIndexSubscriber = new PrintSubscriber<>("Create Index Result: %s");
-        collection.createIndex(new Document("i", 1)).subscribe(createIndexSubscriber);
-        createIndexSubscriber.await();
-
-        // Clean up
-        successSubscriber = new OperationSubscriber<>();
-        collection.drop().subscribe(successSubscriber);
-        successSubscriber.await();
-
-        // release resources
-        mongoClient.close();
+        public void setChild(Child child) {
+            this.child = child;
+        }
     }
 }
